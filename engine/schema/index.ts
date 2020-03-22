ab78bee0cb9ab8c481ddbfbbc1a1e2da08720390
Marcel Laverdet <marcel@laverdet.com>
2020-03-21 20:51:28 -0700

    Update package.json to fix an issue that I forgot what it was already

--- engine/schema/index.ts
+++ engine/schema/index.ts
@@ -1,10 +1,9 @@
 export { BufferView } from './buffer-view';
-export { makeArray, makeVariant, makeVector, withType, Format, Inherit, Variant } from './format';
-export { Interceptor } from './interceptor';
+export { makeArray, makeVariant, makeVector, withType, Inherit, Variant } from './format';
+export type { Format } from './format';
+export type { Interceptor } from './interceptor';
 export { getReader } from './read';
 export { getWriter } from './write';
-
-//export type Shape<Layout, Interceptors> = ReturnType<getReader<Layout, Interceptors>>;
 
 // Exported for convenience
 export { checkCast } from '~/lib/utility';
