98e8d051dadec5d753eec8f72be33b37f7577421
Marcel Laverdet <marcel@laverdet.com>
2021-06-01 00:52:16 -0700

    Freeze important structures in isolated-vm mode

--- src/driver/sandbox/isolated/runtime.ts
+++ src/driver/sandbox/isolated/runtime.ts
@@ -3,6 +3,44 @@ import type { InitializationPayload } from 'xxscreeps/driver';
 import * as Runtime from 'xxscreeps/driver/runtime';
 export { tick } from 'xxscreeps/driver/runtime';
 
+function freezeClass(constructor: abstract new(...args: any[]) => any) {
+	freezeProperty(constructor, 'prototype');
+	for (
+		let prototype = constructor.prototype;
+		prototype !== null;
+		prototype = Object.getPrototypeOf(prototype)
+	) {
+		Object.freeze(prototype);
+	}
+}
+
+function freezeProperty(object: {}, key: keyof any) {
+	const info = Object.getOwnPropertyDescriptor(object, key)!;
+	info.configurable = false;
+	info.writable = false;
+	Object.defineProperty(object, key, info);
+}
+
+// `iterator` can be used to override the behavior of the spread operator
+freezeProperty(Array.prototype, Symbol.iterator);
+
+// These all need to be locked down to prevent write access to shared terrain state
+const typedArrays = [
+	'ArrayBuffer',
+	'SharedArrayBuffer',
+	'Uint8Array',
+	'Uint16Array',
+	'Uint32Array',
+	'Int8Array',
+	'Int16Array',
+	'Int32Array',
+	'Float64Array',
+] as const;
+for (const key of typedArrays) {
+	freezeProperty(globalThis, key);
+	freezeClass(globalThis[key]);
+}
+
 export function initialize(
 	isolate: ivm.Isolate,
 	context: ivm.Context,
