970683a33e4403b8da073c79d76303f98637dac0
Marcel Laverdet <marcel@laverdet.com>
2021-08-08 00:27:31 -0500

    Include Steam OpenID support

---
+++ src/mods/backend/steam/ticket.ts
@@ -0,0 +1,42 @@
+import { hooks } from 'xxscreeps/backend';
+import fetch from 'node-fetch';
+import config from 'xxscreeps/config';
+
+const { steamApiKey } = config.backend;
+if (steamApiKey) {
+	hooks.register('route', {
+		method: 'post',
+		path: '/api/auth/steam-ticket',
+
+		async execute(context) {
+			// Native auth not implemented, get an API key!
+			if (context.query.useNativeAuth !== undefined) {
+				context.status = 501;
+				return;
+			}
+
+			// Get user id from Steam
+			const response = await fetch(`https://api.steampowered.com/ISteamUserAuth/AuthenticateUserTicket/v1/?${new URLSearchParams({
+				key: steamApiKey,
+				appid: `${464350}`,
+				ticket: context.request.body.ticket,
+			})}`);
+			if (response.status === 200) {
+				const { result, steamid } = (await response.json())?.response?.params ?? {};
+
+				if (result !== 'OK') {
+					throw new Error('Steam authentication failure');
+				}
+
+				// Respond with temporary token. auth/me handles upgrading token to user
+				await context.authenticateForProvider('steam', steamid);
+				return {
+					ok: 1,
+					token: await context.flushToken(),
+				};
+			} else {
+				throw new Error('Steam failure. Check `backend.steamApiKey`');
+			}
+		},
+	});
+}
