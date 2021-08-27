bd9642060aa2ff316e52d3412acd75822411cae6
Gadjung <gadjung@gmail.com>
2021-08-27 09:34:03 +0200

    Update of move tests

--- src/mods/creep/test.ts
+++ src/mods/creep/test.ts
@@ -190,21 +190,149 @@ describe("Movement", () => {
         );
       });
     }));
-  const moveThrough = simulate({
+	
+  const enterHostileTakenPosition = simulate({
     W2N2: (room) => {
       room["#level"] = 1;
       room["#user"] = room.controller!["#user"] = "100";
       room["#safeModeUntil"] = 100;
-      room["#insertObject"](
-        create(new RoomPosition(1, 1, "W2N2"), [C.MOVE], "owner", "100")
-      );
-      room["#insertObject"](
-        create(new RoomPosition(1, 2, "W2N2"), [C.MOVE], "hostile", "101")
-      );
+      room["#insertObject"](create(new RoomPosition(20, 20, "W2N2"), [C.MOVE], "owner", "100"));
+      room["#insertObject"](create(new RoomPosition(20, 21, "W2N2"), [C.MOVE], "hostile", "101"));
     },
   });
-  test("safe mode walk-through hostile creep", () =>
-    moveThrough(async ({ player, tick }) => {
+  test("safe mode - owner enters position occupied by hostile", () =>
+    enterHostileTakenPosition(async ({ player, tick }) => {
+      await player("100", (Game) => {
+        assert.strictEqual(Game.rooms.W2N2.controller?.safeMode != undefined, true);
+      });
+      await player("100", (Game) => {
+        assert.ok(Game.creeps.owner.pos.isEqualTo(new RoomPosition(20, 20, "W2N2")));
+        assert.strictEqual(Game.creeps.owner.move(C.BOTTOM), C.OK);
+      });
+      await tick();
+      await player("100", (Game) => {
+        assert.ok(Game.creeps.owner.pos.isEqualTo(new RoomPosition(20, 21, "W2N2")));
+      });
+    }));
+
+  const enterSameTile = simulate({
+    W2N2: (room) => {
+      room["#level"] = 1;
+      room["#user"] = room.controller!["#user"] = "100";
+      room["#safeModeUntil"] = 100;
+      room["#insertObject"](create(new RoomPosition(20, 20, "W2N2"), [C.MOVE], "owner", "100"));
+      room["#insertObject"](create(new RoomPosition(20, 22, "W2N2"), [C.MOVE], "hostile", "101"));
+    },
+  });
+  test("safe mode - owner&hostile try to enter same empty tile - owner moves", () =>
+    enterSameTile(async ({ player, tick }) => {
+      await player("100", (Game) => {
+        assert.strictEqual(Game.rooms.W2N2.controller?.safeMode != undefined, true);
+      });
+      await player("100", (Game) => {
+        assert.ok(Game.creeps.owner.pos.isEqualTo(new RoomPosition(20, 20, "W2N2")));
+        assert.strictEqual(Game.creeps.owner.move(C.BOTTOM), C.OK);
+      });
+      await player("101", (Game) => {
+        assert.ok(Game.creeps.hostile.pos.isEqualTo(new RoomPosition(20, 22, "W2N2")));
+        assert.strictEqual(Game.creeps.hostile.move(C.TOP), C.OK);
+      });
+      await tick();
+      await player("100", (Game) => {
+        assert.ok(Game.creeps.owner.pos.isEqualTo(new RoomPosition(20, 21, "W2N2")));
+      });
+      await player("101", (Game) => { 
+        assert.ok(Game.creeps.hostile.pos.isEqualTo(new RoomPosition(20, 22, "W2N2")));
+      });
+    }));
+    
+    
+  const enterSameTileOwnerObstacle = simulate({
+    W2N2: (room) => {
+      room["#level"] = 1;
+      room["#user"] = room.controller!["#user"] = "100";
+      room["#safeModeUntil"] = 100;
+      room["#insertObject"](create(new RoomPosition(20, 20, "W2N2"), [C.MOVE], "owner", "100"));
+      room["#insertObject"](create(new RoomPosition(20, 21, "W2N2"), [C.MOVE], "ownerObstacle", "100"));
+      room["#insertObject"](create(new RoomPosition(20, 22, "W2N2"), [C.MOVE, C.MOVE], "hostile2", "101"));
+    },
+  });
+  test("safe mode - owner&hostile want to enter a ownerObstacle position - noone moves", () =>
+    enterSameTileOwnerObstacle(async ({ player, tick }) => {
+      await player("100", (Game) => {
+        assert.strictEqual(Game.rooms.W2N2.controller?.safeMode != undefined, true);
+      });
+      await player("100", (Game) => {
+        assert.ok(Game.creeps.ownerObstacle.pos.isEqualTo(new RoomPosition(20, 21, "W2N2")));
+        assert.ok(Game.creeps.owner.pos.isEqualTo(new RoomPosition(20, 20, "W2N2")));
+        assert.strictEqual(Game.creeps.owner.move(C.BOTTOM), C.OK);
+      });
+      await player("101", (Game) => {
+        assert.ok(Game.creeps.hostile2.pos.isEqualTo(new RoomPosition(20, 22, "W2N2")));
+        assert.strictEqual(Game.creeps.hostile2.move(C.TOP), C.OK);
+      });
+      await tick();
+      await player("100", (Game) => {
+        assert.ok(Game.creeps.ownerObstacle.pos.isEqualTo(new RoomPosition(20, 21, "W2N2")));
+        assert.ok(Game.creeps.owner.pos.isEqualTo(new RoomPosition(20, 20, "W2N2")));
+      });
+      await player("101", (Game) => {
+        assert.ok(Game.creeps.hostile2.pos.isEqualTo(new RoomPosition(20, 22, "W2N2")));
+      });
+    }));
+
+  const enterSameTileHostileObstacle = simulate({
+    W2N2: (room) => {
+      room["#level"] = 1;
+      room["#user"] = room.controller!["#user"] = "100";
+      room["#safeModeUntil"] = 100;
+      room["#insertObject"](create(new RoomPosition(20, 20, "W2N2"), [C.MOVE], "owner", "100"));
+      room["#insertObject"](create(new RoomPosition(20, 21, "W2N2"), [C.MOVE], "hostileObstacle", "101"));
+      room["#insertObject"](create(new RoomPosition(20, 22, "W2N2"), [C.MOVE, C.MOVE], "hostile2", "101"));
+    },
+  });
+  test("safe mode - owner&hostile want to enter a hostile2-occupied position - owner moves", () =>
+    enterSameTileHostileObstacle(async ({ player, tick }) => {
+      await player("100", (Game) => {
+        assert.strictEqual(Game.rooms.W2N2.controller?.safeMode != undefined, true);
+      });
+      await player("100", (Game) => {
+        assert.ok(Game.creeps.owner.pos.isEqualTo(new RoomPosition(20, 20, "W2N2")));
+        assert.strictEqual(Game.creeps.owner.move(C.BOTTOM), C.OK);
+      });
+      await player("101", (Game) => {
+        assert.ok(Game.creeps.hostileObstacle.pos.isEqualTo(new RoomPosition(20, 21, "W2N2")));
+        assert.ok(Game.creeps.hostile2.pos.isEqualTo(new RoomPosition(20, 22, "W2N2")));
+        assert.strictEqual(Game.creeps.hostile2.move(C.TOP), C.OK);
+      });
+      await tick();
+      await player("100", (Game) => {
+        assert.ok(Game.creeps.owner.pos.isEqualTo(new RoomPosition(20, 21, "W2N2")));
+      });
+      await player("101", (Game) => {
+        assert.ok(Game.creeps.hostileObstacle.pos.isEqualTo(new RoomPosition(20, 21, "W2N2")));
+        assert.ok(Game.creeps.hostile2.pos.isEqualTo(new RoomPosition(20, 22, "W2N2")));
+      });
+    }));
+
+  const enterPossiblyFreeTile = simulate({
+    W2N2: (room) => {
+      room["#level"] = 1;
+      room["#user"] = room.controller!["#user"] = "100";
+      room["#safeModeUntil"] = 100;
+      room["#insertObject"](create(new RoomPosition(20, 20, "W2N2"), [C.MOVE], "owner", "100"));
+      room["#insertObject"](create(new RoomPosition(19, 21, "W2N2"), [C.MOVE], "ownerObstacle", "100"));
+      room["#insertObject"](create(new RoomPosition(20, 21, "W2N2"), [C.MOVE], "hostile", "101"));
+      room["#insertObject"](create(new RoomPosition(20, 22, "W2N2"), [C.MOVE, C.MOVE], "hostile2", "101"));
+    },
+  });
+  test("safe mode - owner&hostile2 try to enter position occupied by hostile, who try to move to blocked position - owner moves", () =>
+    enterPossiblyFreeTile(async ({ player, tick }) => {
+    // tick1
+    // ownerObstacle does not move
+    // onwer -> hostile (success -> room is in safemode so can walk on-top of enemy player creeps)
+    // hostile -> ownerObstacle (fail -> ownerObstacle in place)
+    // hostile2 -> hostile (fail -> hostile in position )
       await player("100", (Game) => {
         assert.strictEqual(
           Game.rooms.W2N2.controller?.safeMode != undefined,
@@ -212,34 +340,32 @@ describe("Movement", () => {
         );
       });
       await player("100", (Game) => {
-        assert.ok(
-          Game.creeps.owner.pos.isEqualTo(new RoomPosition(1, 1, "W2N2"))
-        );
-        assert.strictEqual(Game.creeps.owner.move(C.BOTTOM), C.OK);
-      });
-      await tick();
-      await player("100", (Game) => {
-        assert.ok(
-          Game.creeps.owner.pos.isEqualTo(new RoomPosition(1, 2, "W2N2"))
-        );
+        assert.ok(Game.creeps.ownerObstacle.pos.isEqualTo(new RoomPosition(19, 21, "W2N2")));
+        
+        // try to move into hostile position
+        assert.ok(Game.creeps.owner.pos.isEqualTo(new RoomPosition(20, 20, "W2N2")));
         assert.strictEqual(Game.creeps.owner.move(C.BOTTOM), C.OK);
       });
       await player("101", (Game) => {
-        assert.ok(
-          Game.creeps.owner.pos.isEqualTo(new RoomPosition(1, 2, "W2N2"))
-        );
-        assert.strictEqual(Game.creeps.hostile.move(C.BOTTOM), C.OK);
+        // try to move into ownerObstacle position
+        assert.ok(Game.creeps.hostile.pos.isEqualTo(new RoomPosition(20, 21, "W2N2")));
+        assert.strictEqual(Game.creeps.hostile.move(C.LEFT), C.OK);
+        
+        // try to move into hostile position
+        assert.ok(Game.creeps.hostile2.pos.isEqualTo(new RoomPosition(20, 22, "W2N2")));
+        assert.strictEqual(Game.creeps.hostile2.move(C.TOP), C.OK);
       });
       await tick();
-      await player("100", (Game) => {
-        assert.ok(
-          Game.creeps.owner.pos.isEqualTo(new RoomPosition(1, 3, "W2N2"))
-        );
-      });
       await player("101", (Game) => {
-        assert.ok(
-          Game.creeps.owner.pos.isEqualTo(new RoomPosition(1, 3, "W2N2"))
-        );
+        // hostile&hostile2 did not move
+        assert.ok(Game.creeps.hostile.pos.isEqualTo(new RoomPosition(20, 21, "W2N2")));
+        assert.ok(Game.creeps.hostile2.pos.isEqualTo(new RoomPosition(20, 22, "W2N2")));
+      });
+      await player("100", (Game) => {
+        // owner moved to hostile position
+        assert.ok(Game.creeps.owner.pos.isEqualTo(new RoomPosition(20, 21, "W2N2")), `Owner.pos:${Game.creeps.owner.pos} should be ${new RoomPosition(20, 21, "W2N2")}`);
+        assert.ok(Game.creeps.owner2.pos.isEqualTo(new RoomPosition(19, 21, "W2N2")));
       });
     }));
 });
+
