8d59751a90aa7bf28de8afd35314626bf371ec73
Bastian Hoyer <dafire@gmail.com>
2021-09-11 21:50:56 +0200

    fix eslint warnings

--- src/mods/observer/game.ts
+++ src/mods/observer/game.ts
@@ -1,16 +1,16 @@
-import {registerVariant} from "xxscreeps/engine/schema";
-import {registerGlobal} from "xxscreeps/game";
-import * as Observer from './observer'
+import { registerVariant } from 'xxscreeps/engine/schema';
+import { registerGlobal } from 'xxscreeps/game';
+import * as Observer from './observer';
 
 // Register schema
 const observerSchema = registerVariant('Room.objects', Observer.format);
 declare module 'xxscreeps/game/room' {
-    interface Schema { observer: [ typeof observerSchema ] }
+	interface Schema { observer: [ typeof observerSchema ] }
 }
 
 registerGlobal(Observer.StructureObserver);
 declare module 'xxscreeps/game/runtime' {
-    interface Global {
-        StructureObserver: typeof Observer.StructureObserver;
-    }
+	interface Global {
+		StructureObserver: typeof Observer.StructureObserver;
+	}
 }
