86715d4bc99dabafb578ab7e00ede5aab9d24c97
thmsndk <9084377+thmsndk@users.noreply.github.com>
2022-01-17 08:22:43 +0100

    tests for moving onto ramparts (#51)

---
+++ src/mods/defense/test.ts
@@ -0,0 +1,46 @@
+import * as C from 'xxscreeps/game/constants';
+import { assert, describe, simulate, test } from 'xxscreeps/test';
+import { RoomPosition } from 'xxscreeps/game/position';
+import { create as createConstructionSite } from 'xxscreeps/mods/construction/construction-site';
+import { create as createCreep } from 'xxscreeps/mods/creep/creep';
+
+describe('ramparts', () => {    
+	const roomWithUnbuiltRamparts = simulate({
+		W0N0: room => {
+			room['#level'] = 3;
+			room['#user'] = '100';
+			room['#insertObject'](createCreep(new RoomPosition(24, 25, 'W0N0'), [ C.MOVE ], 'rampart_movement', '100'));
+			room['#insertObject'](createConstructionSite(new RoomPosition(25, 25, 'W0N0'), 'rampart', '100'));
+		},
+	});
+
+	test('moveTo should be able to pass trough rampart csite', () => roomWithUnbuiltRamparts(async({ player, tick }) => {
+		await player('100', Game => {
+			assert.strictEqual(Game.creeps.rampart_movement.moveTo(25, 25), C.OK);
+		});
+        
+		await tick();
+
+		await player('100', Game => {
+			const pos = Game.creeps.rampart_movement.pos;
+			const { x, y } = pos;
+			assert.strictEqual(x, 25);
+			assert.strictEqual(y, 25);
+		});
+	}));
+
+	test('move should be able to pass trough rampart csite', () => roomWithUnbuiltRamparts(async({ player, tick }) => {
+		await player('100', Game => {
+			assert.strictEqual(Game.creeps.rampart_movement.move(C.RIGHT), C.OK);
+		});
+        
+		await tick();
+
+		await player('100', Game => {
+			const pos = Game.creeps.rampart_movement.pos;
+			const { x, y } = pos;
+			assert.strictEqual(x, 25);
+			assert.strictEqual(y, 25);
+		});
+	}));
+});
