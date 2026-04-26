laverdet/xxscreeps

commit fc98dc9411c4b444e11ec469024c6dabe5b992f6

Simplify entry script

packages/xxscreeps/config/entry.ts
@@ -53,7 +53,6 @@ if (missingFlags.length) {
 } else {
 
 	// All required flags were passed
-
 	if (!isMainThread && workerData?.isTopThread) {
 		// This is a fake top-thread, so the real top thread will send SIGINT messages
 		parentPort!.on('message', message => {
@@ -65,19 +64,9 @@ if (missingFlags.length) {
 		}).unref();
 	}
 
-	// `registerStorageProvider` needs to be imported early to allow local keyval/blob providers to
-	// register
-	await import('xxscreeps/engine/db/storage/register.js');
-
 	// Get script and remove `dist/config/entry.js` from args
 	process.argv.splice(1, 1);
 	const specifier = process.argv[1];
-
-	// Load mods
-	await Promise.all([
-		import('./mods/index.js'),
-		import('./global.js'),
-	]);
 
 	if (specifier) {
 		const commands: Record<string, string | undefined> = {

packages/xxscreeps/config/mods/import/config.ts
@@ -1,5 +1,6 @@
 import * as Config from 'xxscreeps/config/config.js';
 
+// It's in package.json
 throw new Error('nodejs is misconfigured');
 
 const mods = [ Config ];

packages/xxscreeps/config/mods/import/constants.ts
@@ -1,5 +1,6 @@
 // All constants from mods are added here for TypeScript's eyes only. nodejs will include
 // `constants.ts`.
+// It's in package.json
 throw new Error('nodejs is misconfigured');
 export * from 'xxscreeps/mods/chemistry/constants.js';
 export * from 'xxscreeps/mods/combat/constants.js';

packages/xxscreeps/config/mods/import/game.ts
@@ -1,2 +1,3 @@
 export {};
+// It's in package.json
 throw new Error('nodejs is misconfigured');

packages/xxscreeps/engine/service/main.ts
@@ -93,7 +93,9 @@ try {
 					config.processor.intentAbandonTimeout,
 					() => mustNotReject(async () => {
 						const rooms = await abandonIntentsForTick(shard, time);
-						console.log(`Abandoning intents in rooms [${rooms.join(', ')}] for tick ${time}`);
+						if (rooms.length > 0) {
+							console.log(`Abandoning intents in rooms [${rooms.join(', ')}] for tick ${time}`);
+						}
 					}));
 				for await (const message of serviceMessages) {
 					if (message.type === 'processorInitialized') {

commit fed673c675139a7918402c0789c5c2384bfc3e67

Fix the tests
Broken in fc98dc9411

packages/xxscreeps/config/entry.ts
@@ -68,6 +68,9 @@ if (missingFlags.length) {
 	process.argv.splice(1, 1);
 	const specifier = process.argv[1];
 
+	// Load mods (regenerates mods.static)
+	await import('./mods/index.js');
+
 	if (specifier) {
 		const commands: Record<string, string | undefined> = {
 			import: './dist/scripts/scrape-world.js',