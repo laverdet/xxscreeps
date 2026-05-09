6cabc5cb1a338618b9f657302cea0fb7d24317a6
Marcel Laverdet <marcel@laverdet.com>
2026-05-09 17:53:30 +0200

    Fix FIND_HOSTILE_STRUCTURES
    Resets the implementation of .my to what it was before 2830cf4ffd.
    Tests from #193
    Slightly adjusted FIND_HOSTILE_STRUCTURES implementation

--- packages/xxscreeps/mods/structure/game.ts
+++ packages/xxscreeps/mods/structure/game.ts
@@ -27,7 +27,7 @@ const find = registerFindHandlers({
 	[C.FIND_MY_STRUCTURES]: room =>
 		room['#lookFor'](C.LOOK_STRUCTURES).filter(structure => structure.my) as OwnedStructure[],
 	[C.FIND_HOSTILE_STRUCTURES]: room =>
-		room['#lookFor'](C.LOOK_STRUCTURES).filter(structure => !structure.my) as OwnedStructure[],
+		room['#lookFor'](C.LOOK_STRUCTURES).filter(structure => structure.my === false) as OwnedStructure[],
 	[C.FIND_RUINS]: room => room['#lookFor'](C.LOOK_RUINS),
 });
 
